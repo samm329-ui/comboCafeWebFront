d12dd588c198768f694f9596c4abae86d7705963
Jishu Das <jishudas329@gmail.com>
2026-01-29 10:24:27 +0000

    remove the firebase logo from the website tab and add a other icon accor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata = {
   title: 'Combo Cafe and Gift Shop',
   description: 'Your one-stop destination for delightful cakes, thoughtful gifts, and delicious food in Rampurhat.',
   icons: {
-    icon: 'https://gpfocwgfedokhmfsbcpy.supabase.co/storage/v1/object/public/asset/image%20(1).png',
+    icon: 'https://gpfocwgfedokhmfsbcpy.supabase.co/storage/v1/object/public/asset/buttons/cake%20.png',
   },
 };
 
